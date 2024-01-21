76b2309266b9a7cdc627cb5696892407d679256c
Carlos <44345544+Werkfakter@users.noreply.github.com>
2024-01-21 10:19:37 -0800

    Fix typo
    Removed 'Valorous Rage' from 'Spire of Chivalry'

--- class_configs/pal_class_config.lua
+++ class_configs/pal_class_config.lua
@@ -735,7 +735,7 @@ return {
     ['Rotations']         = {
         ['Burn'] = {
             {
-                name = "Spire of ChivalryValorous Rage",
+                name = "Spire of Chivalry",
                 type = "AA",
                 cond = function(self, aaName)
                     return mq.TLO.Me.Level() < 80 and RGMercUtils.AAReady(aaName) and not RGMercUtils.SongActive('Group Armor of the Inquisitor') and
